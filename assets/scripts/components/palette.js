d6a4c2e47869424a8c55222bd87635bdaf5bde8c
Habeeb4shittu <habeeb4shittu@gmail.com>
2023-05-12 14:57:03 -0700

    advanced

--- assets/scripts/components/palette.js
+++
@@ -1,9 +0,0 @@
-export function palette(color1, color2)
-{
-    $("main").append(`
-    <div class="palette"></div>
-    `)
-    $(".palette").css({
-        background: `linear-gradient(${color1}, ${color2})`
-    })
-}
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,1 @@
-import { inputs } from './components/inputs.js'
-import { palette } from './components/palette.js'
-
-palette("red", "yellow")
-inputs()
-// $("#tester").on('change', function ()
-// {
-//     console.log($(this).val());
-// })
+$(".color-code").text($(".gradient").css("background"))
